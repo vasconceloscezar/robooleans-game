ebbe2954fabe748dbce03498446564775509a2b4
Cezar Vasconcelos <cezarvasconcelos.dev@gmail.com>
2023-04-05 22:29:40 -0300

    chore: remove husky

---
+++ .eslintrc.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['plugin:react/recommended', 'airbnb'],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'react/react-in-jsx-scope': 'off',
+  },
+  ignorePatterns: ['node_modules/**', '*.json'],
+};
